9d0448a03ae0b0f2b93390c99c4a5148dd1d48c6
Christopher Kenny <ctk44@cornell.edu>
2026-08-21 12:55:47 -0400

    add simplified shapes with clean merge

--- notes/county_data.R
+++ notes/county_data.R
@@ -1,6 +1,13 @@
 library(tidycensus)
 library(tidyverse)
 
+state_lookup <- censable::stata |>
+  mutate(
+    state_fips = stringr::str_pad(as.character(fips), width = 2L, pad = '0'),
+    state = name,
+    .keep = 'none'
+  )
+
 county_data <- get_acs(
   geography = 'county',
   variables = c(
@@ -10,22 +17,47 @@ county_data <- get_acs(
   survey = 'acs5',
   year = 2023L
 ) |>
+  mutate(
+    GEOID = stringr::str_pad(as.character(GEOID), width = 5L, pad = '0'),
+    state_fips = stringr::str_sub(GEOID, 1L, 2L)
+  ) |>
   tidyr::pivot_wider(
     names_from = variable,
     values_from = c(estimate, moe),
     names_glue = '{variable}_{.value}'
   ) |>
-  dplyr::rename(
+  rename(
     income = income_estimate,
     rent = rent_estimate
   ) |>
   tidyr::separate(
     NAME,
-    into = c('county', 'state'),
+    into = c('county', 'state_from_acs'),
     sep = ', ',
     remove = FALSE
   ) |>
-  dplyr::select(GEOID, state, county, income, income_moe, rent, rent_moe) |>
-  filter(state != 'Puerto Rico')
+  left_join(state_lookup, by = 'state_fips') |>
+  filter(state != 'Puerto Rico') |>
+  select(GEOID, state, county, income, income_moe, rent, rent_moe)
 
 write_csv(county_data, here::here('files/county_data.csv'))
+
+
+shp <- tigris::counties(cb = TRUE, resolution = '20m') |>
+  filter(STUSPS %in% c(state.abb, 'DC')) |>
+  mutate(
+    GEOID = stringr::str_pad(as.character(GEOID), width = 5L, pad = '0'),
+    state_fips = stringr::str_sub(GEOID, 1L, 2L),
+    county = NAMELSAD
+  ) |>
+  left_join(state_lookup, by = 'state_fips') |>
+  tigris::shift_geometry() |>
+  rmapshaper::ms_simplify(keep_shapes = TRUE, keep = 0.1) |>
+  select(GEOID, state, county, geometry)
+
+sf::st_write(
+  shp,
+  here::here('files', 'county_shapes.geojson'),
+  delete_dsn = TRUE,
+  quiet = TRUE
+)
